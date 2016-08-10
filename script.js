b452f6e3c063fd5392edc66a2278c7d86b4b6bec
sanya <sanya@MacBook-Pro.local>
2016-08-10 17:35:34 +0300

    align elements

--- script.js
+++ script.js
@@ -5,20 +5,55 @@
 			this.wrapper = this.container.querySelector('.sl-wrapper');
 
 			this.setSlider(this.wrapper);
+
+			setInterval(this.setSlider.bind(this, this.wrapper), 3000)
 		}
-		setSLider(wrapper){
+		setSlider(wrapper){
+			let left = 0;
 			let elems = wrapper.querySelectorAll('.sl-elements');
+
+			for(let i = 0; i < elems.length; i++) {
+				console.log(left);
+				elems[i].style.left = left + 'px';
+
+				let elemWIidth = this.elementSize(elems[i]).width;
+
+				left += elemWIidth;
+
+				
+			}
+		}
+
+		getScrollWidth(){
+		  let div = document.createElement('div');
+
+		  div.style.overflowY = 'scroll';
+		  div.style.width = '50px';
+		  div.style.height = '50px';
+		  div.style.visibility = 'hidden';
+		  div.style.position = 'absolute';
+		  div.style.top = '0px';
+		  div.style.left = '0px';
+
+
+
+		  document.body.appendChild(div);
+		  let scrollWidth = div.offsetWidth - div.clientWidth;
+		  let scrollHeight = div.offsetHeight - div.clientHeight;
+		  document.body.removeChild(div);
+
+		  return {width: scrollWidth, height: scrollHeight};
 		}
 
 		elementSize(element) {
 
 		  let elem = getComputedStyle(element);
 
-		  this.scrollWidth = this.getScrollWidth();
+		  // this.scrollWidth = this.getScrollWidth();
 
 		  return {
-		    width: parseInt(elem.width, 10) + parseInt(elem.marginLeft, 10) + parseInt(elem.marginRight, 10) + this.scrollWidth.width,
-		    height: parseInt(elem.height, 10) + parseInt(elem.marginBottom, 10) + parseInt(elem.marginTop, 10) + +this.scrollWidth.height
+		    width: parseInt(elem.width, 10) + parseInt(elem.marginLeft, 10) + parseInt(elem.marginRight, 10),// + this.scrollWidth.width,
+		    height: parseInt(elem.height, 10) + parseInt(elem.marginBottom, 10) + parseInt(elem.marginTop, 10)// + this.scrollWidth.height
 		  }
 
 		}
